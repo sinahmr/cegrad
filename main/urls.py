ae305156c5b9a7c9a8d9a09aac6396e92610a90c
Sina Hajimiri <sina.hajimiri@gmail.com>
2018-03-19 15:19:28 +0330

    Statics are added

--- main/urls.py
+++ main/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import url
+from main import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('main.urls'))
-] + static(settings.STATIC_URL)
+    url(r'q/', views.question)
+]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
+from random import choice
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
 from main.models import TheMost
 
 
 @login_required
 def question(request):
     voter = request.user
-    a = TheMost.objects.filter(vote__voter=voter)
-    print(a)
-    return render('<div/>')
+    if request.method == 'POST':
+        pass
+    else:
+        remaining_questions = TheMost.objects.exclude(vote__voter=voter)
+        chosen_question = choice(remaining_questions)
+
+        candidates = User.objects.filter(is_superuser=False).exclude(username=voter.username)
+        return render(request, 'main/question.html')
+
