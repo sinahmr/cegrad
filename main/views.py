4fbe4fbd56b1ba590aa3f26df946842534ad7d23
Sina Hajimiri <sina.hajimiri@gmail.com>
2018-03-19 16:23:08 +0330

    Better question page

--- main/views.py
+++ main/views.py
@@ -1,20 +1,30 @@
 from random import choice
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
 
-from main.models import TheMost
+from main.models import *
 
 
 @login_required
 def question(request):
     voter = request.user
     if request.method == 'POST':
-        pass
+        candidate = get_object_or_404(User, username=request.POST.get('candidate'))
+        q = get_object_or_404(TheMost, pk=request.POST.get('question_id'))
+        try:
+            Vote.objects.create(voter=voter, candidate=candidate, the_most=q)
+        except IntegrityError:
+            return HttpResponseBadRequest('You have already voted')
+        return HttpResponseRedirect(request.path)
     else:
         remaining_questions = TheMost.objects.exclude(vote__voter=voter)
-        chosen_question = choice(remaining_questions)
+        if remaining_questions.count() > 0:
+            chosen_question = choice(remaining_questions)
+        else:
+            chosen_question = None
 
         candidates = User.objects.filter(is_superuser=False).exclude(username=voter.username)
         return render(request, 'main/question.html', {
