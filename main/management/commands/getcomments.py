806c58b9645752b1c4cbaa63d9e0a98f68ad9790
Mohammad <sadeghtaghidizaj@ce.sharif.ir>
2018-05-03 21:59:16 +0430

    prepare for latex

--- main/management/commands/getcomments.py
+++ main/management/commands/getcomments.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from main.models import *
 import xlwt
 import os
-
+import re
 
 class Command(BaseCommand):
     help = "get comments"
@@ -35,25 +35,40 @@ class Command(BaseCommand):
                     sheet.write(i+1, 0, comment.id)
                     sheet.write(i+1, 1, comment.commenter.get_name())
                     sheet.write(i+1, 2, comment.target.get_name())
-                    sheet.write(i+1, 3, comment.text)
+                    sheet.write(i+1, 3, f2(f3(f1(f4(comment.text)))))
                     # print(comment.id, comment.commenter.get_name())
                 name = os.path.join('comments', user.username + '.xls')
                 book.save(name)
         print('OK')
 
-#
-# def create_valid_text(text):
-#     # text = text.replace("\\", "\\textbackslash")
-#     text = text.replace("#", "\#")
-#     text = text.replace("^", "\^")
-#     text = text.replace("_", "\_")
-#     text = text.replace("@", "\@")
-#     text = text.replace("&", "\&")
-#     text = text.replace("$", "\$")
-#     text = text.replace("~", "\\textasciitilde")
-#     text = text.replace("\n", "\n\n")
-#     file = open('test.txt', 'w')
-#     file.write(text)
-#     file.close()
-#     return text
-#
+
+def f1(input):
+    output = input
+    pattern = '\^_+\^'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = '\lstinline!%s!' % x
+        output = output.replace(x, y)
+    return output
+
+
+def f2(input):
+    output = input
+    pattern = '\n'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = r' \newline '
+        output = output.replace(x, y)
+    return output
+
+
+def f3(input):
+    output = input.replace('#', '\#').replace("@", "\@").replace("&", "\&").replace("$", "\$").replace("~", r"\textasciitilde ")
+    return output
+
+
+def f4(input):
+    output = input
+    pattern = '([a-zA-Z\d,#?]+[a-zA-Z\d,\s,<>,.;:!?@#$_$)(]*[a-zA-Z\d,\s,:,.!?)(]+)'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = '\lr{ %s }' % x
+        output = output.replace(x, y)
+    return output
--- main/management/commands/getopinions.py
+++ main/management/commands/getopinions.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from main.models import *
 import xlwt
 import os
-
+import re
 
 class Command(BaseCommand):
     help = "get opinions"
@@ -33,9 +33,41 @@ class Command(BaseCommand):
                 for i, opinion in enumerate(opinions):
                     sheet.write(i+1, 0, opinion.id)
                     sheet.write(i+1, 1, opinion.teller.get_name())
-                    sheet.write(i+1, 2, opinion.subject)
-                    sheet.write(i+1, 3, opinion.text)
+                    sheet.write(i+1, 2, f2(f3(f1(f4(opinion.text)))))
+                    sheet.write(i+1, 3, f2(f3(f1(f4(opinion.text)))))
 
                 name = os.path.join('opinions', user.username + '.xls')
                 book.save(name)
         print('OK')
+
+
+def f1(input):
+    output = input
+    pattern = '\^_+\^'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = '\lstinline!%s!' % x
+        output = output.replace(x, y)
+    return output
+
+
+def f2(input):
+    output = input
+    pattern = '\n'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = r' \newline '
+        output = output.replace(x, y)
+    return output
+
+
+def f3(input):
+    output = input.replace('#', '\#').replace("@", "\@").replace("&", "\&").replace("$", "\$").replace("~", r"\textasciitilde ")
+    return output
+
+
+def f4(input):
+    output = input
+    pattern = '([a-zA-Z\d,#?]+[a-zA-Z\d,\s,<>,.;:!?@#$_$)(]*[a-zA-Z\d,\s,:,.!?)(]+)'
+    for x in set(re.findall(pattern, input, re.MULTILINE)):
+        y = '\lr{ %s }' % x
+        output = output.replace(x, y)
+    return output
